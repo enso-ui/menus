508e61c5952e6cf2512f60cf39e56710b74bf26b
mishu <mishu@xtelecom.ro>
2026-04-12 15:56:25 +0300

    Register sidebar settings item from menus

---
+++ src/bulma/components/settings/SidebarState.vue
@@ -0,0 +1,32 @@
+<template>
+    <core-sidebar-state>
+        <template #default="{ bindings, events }">
+            <div class="level is-mobile">
+                <div class="level-left">
+                    <div class="level-item">
+                        {{ i18n('Expanded Menu') }}
+                    </div>
+                </div>
+                <div class="level-right">
+                    <div class="level-item">
+                        <vue-switch v-bind="bindings"
+                            v-on="events"/>
+                    </div>
+                </div>
+            </div>
+        </template>
+    </core-sidebar-state>
+</template>
+
+<script>
+import VueSwitch from '@enso-ui/switch/bulma';
+import { SidebarState as CoreSidebarState } from '@enso-ui/ui/renderless/components/settings';
+
+export default {
+    name: 'SidebarState',
+
+    components: { CoreSidebarState, VueSwitch },
+
+    inject: ['i18n'],
+};
+</script>
--- src/bulma/register.js
+++ src/bulma/register.js
@@ -1,5 +1,7 @@
 import MenuOrganizer from './components/settings/MenuOrganizer.vue';
+import SidebarState from './components/settings/SidebarState.vue';
 
 export default App => {
     App.registerSettingsItem('menu-organizer', MenuOrganizer, 400, 'system.menus.organize');
+    App.registerSettingsItem('sidebar-state', SidebarState, 500);
 };
