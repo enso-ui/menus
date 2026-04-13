f3e1a60f849a40ee26b9c9daff8c6035d9369157
mishu <mishu@xtelecom.ro>
2026-04-13 11:35:59 +0300

    Align settings item order

--- src/bulma/register.js
+++ src/bulma/register.js
@@ -2,6 +2,6 @@ import MenuOrganizer from './components/settings/MenuOrganizer.vue';
 import SidebarState from './components/settings/SidebarState.vue';
 
 export default App => {
-    App.registerSettingsItem('menu-organizer', MenuOrganizer, 400, 'system.menus.organize');
-    App.registerSettingsItem('sidebar-state', SidebarState, 500);
+    App.registerSettingsItem('sidebar-state', SidebarState, 400);
+    App.registerSettingsItem('menu-organizer', MenuOrganizer, 600, 'system.menus.organize');
 };
